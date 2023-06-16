4020ef25d84c261401958fb64349b07e645e2b6a
allramos <allanjheyson@hotmail.com>
2023-06-16 14:45:14 -0300

    Add files via upload

---
+++ avalicao_segunda_chamada.sql
@@ -0,0 +1,11 @@
+-- Um cliente realiza várias encomendas. Uma encomenda diz respeito apenas a um cliente.
+
+-- 1. Selecionar todas as encomendas realizadas pelo cliente de ID 8.
+
+-- 2. Selecionar o nome do cliente que realizou a encomenda de ID 286.
+
+-- 3. Selecionar o total de encomendas realizadas pelo cliente "João"
+
+-- 4. Selecionar o nome e telefone de todos os clientes.
+
+-- 5. Selecionar os dados de todas as encomendas.
